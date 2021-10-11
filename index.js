9873b7a531d3ecc9f95f0fc5afd16cc91492b855
Mallory Shea <malloryshea@Mallorys-MacBook-Air.local>
2021-10-11 17:26:59 -0500

    tasks 1-4

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // ⭐️ Example Challenge START ⭐️
 
+const { classBody } = require("@babel/types");
+
 /**Example Task : processFirstItem()
  * This example shows how you might go about solving the rest of the tasks
  * 
@@ -29,11 +31,17 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   
   1. What is the difference between counter1 and counter2?
   
+  One has a closure and one does not.
+
   2. Which of the two uses a closure? How can you tell?
   
+The first one has a closure 
+
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
 */
+
+
 
 // counter1 code
 function counterMaker() {
@@ -62,8 +70,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random() * Math.floor(3));
 }
 
 
@@ -81,17 +89,30 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningCB, innings){
+  let homeScore = 0;
+  let awayScore = 0;
+  for(let i = 0; i< innings; i++){
+    homeScore = homeScore + inningCB();
+    awayScore = awayScore =inningCB();
+  }
+  return{
+    Home:homeScore,
+    Away:awayScore
+  }
 }
+
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB, ){
+  return{
+    Home: inningCB(),
+    Away: inningCB()
+  }
 }
 
 
